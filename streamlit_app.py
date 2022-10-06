2b781da3e15e12bc42939a3bbc66247f59c3a6ca
RhysIrvine <98472662+RhysIrvine@users.noreply.github.com>
2022-10-06 14:59:12 +0100

    Update streamlit_app.py
    imported pandas and added a pandas.read_csv line to display some more information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Healthy Diner')
 streamlit.header('Breakfast  Favorites')
@@ -6,3 +7,6 @@ streamlit.text('🥣Protein Punch')
 streamlit.text('🐔Yoked Egg')
 streamlit.text('🥗Klassic Kale')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
