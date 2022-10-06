0112494bec47c2038886c3c2ff6455e3778581e0
RhysIrvine <98472662+RhysIrvine@users.noreply.github.com>
2022-10-06 14:45:33 +0100

    Update streamlit_app.py
    Added a header and some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Protein Punch')
+streamlit.text('Yoked Egg')
+streamlit.text('Klassic Kale')
